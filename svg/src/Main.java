5bffc7fc988ad613e353db7486e3f6c81adc1009
DomanskiJakub-PL <domanskikuba2005@gmail.com>
2026-03-10 18:10:46 +0100

    Zadanie 5

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,6 +1,7 @@
 import java.sql.SQLOutput;
 
 public class Main {
+
     public static void main(String[] args) {
         Point p1 = new Point();
         p1.x=50.0;
@@ -25,5 +26,35 @@ public class Main {
         System.out.println(s1);
 
         System.out.println(s1.toSvg());
+        Segment[] segments = new Segment[3];
+        segments[0]= new Segment();
+        segments[0].a=new Point();
+        segments[0].b=new Point();
+
+        segments[0].a.x=0;
+        segments[0].a.y=0;
+        segments[0].b.x=0;
+        segments[0].b.y=40;
+        System.out.println(segments[0]);
+
+        segments[1]=s1;
+        segments[1]= new Segment();
+        segments[1].a=new Point();
+        segments[1].b=new Point();
+        segments[1].a.x=0;
+        segments[1].a.y=0;
+        segments[1].b.x=33;
+        segments[1].b.y=0;
+
+        segments[2]= new Segment();
+        segments[2].a=new Point();
+        segments[2].b=new Point();
+        segments[2].a.x=0;
+        segments[2].a.y=0;
+        segments[2].b.x=33;
+        segments[2].b.y=0;
+        Segment max =Segment.maxLength(segments);
+        System.out.println("najdluzszy: "+ max);
+
     }
 }
--- svg/src/Segment.java
+++ svg/src/Segment.java
@@ -12,4 +12,17 @@ public class Segment {
     public String toSvg(){
         return "<line x1=\""+a.x+"\" y1=\""+a.y+"\" x2=\""+b.x+"\" y2=\""+b.y+"\"style=\"stroke:red;stroke-width:2\"   />";
     }
+    public static Segment maxLength(Segment[] segments){
+        if(segments==null || segments.length ==0) return null;
+        Segment max=segments[0];
+        for(int i=0;i< segments.length;i++){
+
+
+            Segment seg= segments[i];
+            if(seg == null) continue;
+            if(seg.leangth()>max.leangth()) return max;
+            max=seg;
+        }
+        return max;
+    };
 }
