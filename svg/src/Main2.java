74fb3b325154db5e25fd45353ac605501308cbf3
DomanskiJakub-PL <domanskikuba2005@gmail.com>
2026-03-10 19:08:58 +0100

    Poligon wojsokwy

--- svg/src/Main2.java
+++ svg/src/Main2.java
@@ -1,2 +1,15 @@
 public class Main2 {
+    public static void main(String[] args) {
+        // wielokąty
+        Point[] verticies = new Point[4];
+        Point v1 = new Point(10,10);
+        verticies[0]=v1;
+        verticies[1]=new Point(50,50);
+        Point v2=new Point(70,10);
+        verticies[2]=v2;
+        verticies[3]=new Point(10,70);
+        Polygon poly =new Polygon(verticies);
+        System.out.println(poly);
+        System.out.println(poly.toSVG());
+    }
 }
--- svg/src/Polygon.java
+++ svg/src/Polygon.java
@@ -1,2 +1,23 @@
+import java.util.Arrays;
+import java.util.Locale;
+
 public class Polygon {
+    private Point[] points;
+    Polygon(Point[] points){
+        this.points=points;
+    }
+
+    @Override
+    public String toString() {
+        return "Polygon{" +
+                "points=" + Arrays.toString(points) +
+                '}';
+    }
+    public String toSVG(){
+        String pointsString="";
+        for(Point p: points){
+            pointsString+=String.format(Locale.ENGLISH,"%f,%f ",p.getX(), p.getY());
+        }
+        return String.format("polygon points=\"%s\"",pointsString);
+    }
 }
